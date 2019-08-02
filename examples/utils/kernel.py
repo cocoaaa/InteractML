0f1fae345a9964ba2afa3cf7a70124b99e23d497
Hayley Song <haejinso@usc.edu>
2019-08-02 10:53:05 -0700

    wip: second-order second derivation finite difference

--- examples/utils/kernel.py
+++ examples/utils/kernel.py
@@ -10,4 +10,9 @@ class CartesianKernel():
     yb = np.atleast_2d([1,-1]).T
     yf = np.atleast_2d([1,-1,0]).T
     
+    diff2_xx = np.atleast2d([1,-2,1])
+    diff2_yy = diff2_xx.T
+    diff2_xy = .25*np.atleast2d([[-1,0,1],
+                             [0,0,0],
+                             [1,0,-1]])
     
